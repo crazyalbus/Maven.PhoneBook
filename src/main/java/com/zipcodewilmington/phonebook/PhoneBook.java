a10c2b2757fc702b291e932a6ebbae9605315a36
Kate Bayard <katherinebayard@zipcoders-MBP-6.lan>
2019-02-16 08:59:57 -0500

    completed drafts of tests an methods

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,51 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
-public class PhoneBook {
+public class PhoneBook<Listings> {
+
+    private TreeMap<String, String> phoneBook = new TreeMap<String, String>();
+
+
+    //methods
+
+    public void addEntry(String name, String phoneNumber) {
+        phoneBook.put(name, phoneNumber);
+    }
+
+    public void removeEntry(String name) {
+        phoneBook.remove(name);
+    }
+
+    public String lookupEntry(String name){
+        return phoneBook.get(name);
+    }
+
+    public String reverseLookupEntry(String phoneNumber) {
+        String lookupName = "";
+        for (Map.Entry<String,String> entry : phoneBook.entrySet() ) {
+            String lookupNumber = phoneNumber;
+            String currentName = entry.getKey();
+            String currentNumber = entry.getValue();
+            if (lookupNumber == currentNumber) {
+                lookupName = currentName;
+            }
+        }
+        return lookupName;
+    }
+
+    public String displayPhoneBook() {
+        String printPhoneBook = "";
+        for (Map.Entry<String,String> entry : phoneBook.entrySet() ) {
+            String currentName = entry.getKey();
+            String currentNumber = entry.getValue();
+            printPhoneBook = printPhoneBook + currentName + currentNumber + "\n";
+        }
+        return printPhoneBook;
+    }
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,94 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.TreeMap;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    String testName1 = "Zebra";
+    String testPhone1 = "111-222-333";
+    String testName2 = "Dog";
+    String testPhone2 = "222-444-4444";
+
+
+    @Test // adds an entry to the composite associate data type
+    public void addEntryTest() {
+        //Given
+        PhoneBook testPhoneBook = new PhoneBook();
+        String testName1 = "Zebra";
+        String testPhone1 = "111-222-333";
+
+        //When
+
+        testPhoneBook.addEntry(testName1, testPhone1);
+
+        //Then
+        Assert.assertTrue(testPhoneBook.containsKey(testName1));
+    }
+
+    @Test // removes an entry to the composite associate data type
+    public void removeEntryTest() {
+        //Given
+        PhoneBook testPhoneBook = new PhoneBook();
+        String testName1 = "Zebra";
+        String testPhone1 = "111-222-333";
+        testPhoneBook.addEntry(testName1, testPhone1);
+
+        //When
+        testPhoneBook.removeEntry(testName1);
+
+        //Then
+        Assert.assertFalse(testPhoneBook.containsKey(testName1));
+    }
+
+    @Test // returns a phone number for the respective input name
+    public void lookupTest() {
+        //Given
+        PhoneBook testPhoneBook = new PhoneBook();
+        String testName1 = "Zebra";
+        String testPhone1 = "111-222-333";
+
+        //When
+        testPhoneBook.lookupEntry(testName1);
+
+        //Then
+        Assert.assertEquals(testPhone1, testPhoneBook.get(testName1));
+    }
+
+
+    @Test // returns a name for the respective input phoneNumber
+    public void reverseLookupTest() {
+        //Given
+        PhoneBook testPhoneBook = new PhoneBook();
+        String testName1 = "Zebra";
+        String testPhone1 = "111-222-333";
+
+        //When
+        testPhoneBook.reverseLookupEntry(testPhone1);
+
+        //Then
+        Assert.assertEquals(testName1, testPhoneBook.get(testPhone1));
+    }
+
+
+    @Test
+    public void displayTest() {
+        //Given
+        PhoneBook testPhoneBook = new PhoneBook();
+        String testName1 = "Zebra";
+        String testPhone1 = "111-222-333";
+
+        //When
+        testPhoneBook.put(testName1, testPhone1);
+        String expectedDisplay = "Dog 222-444-4444\nZebra 111-222-333";
+        String actualDisplay = testPhoneBook.displayPhoneBook();
+
+        //Then
+        Assert.assertEquals(expectedDisplay, actualDisplay);
+    }
 }
